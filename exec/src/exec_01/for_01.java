64c0ea57829e688c6f6c359208622f6e74cb8bbb
min <minkku@github.com>
2023-08-24 16:59:19 +0900

    wip

--- exec/src/exec_01/for_01.java
+++ exec/src/exec_01/for_01.java
@@ -4,37 +4,42 @@ public class for_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int num = 0;
-		
-//		for (num = 0; num < 3; num++) {
-//			System.out.println((Math.random()*3)+1);
-//		}
-//		for (int i = 1; i <= 5; i++) {
-//			System.out.println(i);
-	//	}
-		
-//		for (int j = 1; j <= 5; j++) {
-//			System.out.print(j);
-//		}
-		
-//		for (int k = 0; k < 10; k++ ) {
-//			System.out.println(k);
-//		}
-		
-//		for (int h = 1; h < 11; h++) {
-//			if(h%3 == 0) {
-//				System.out.print(h);
+		//		int num = 0;
+
+		//		for (num = 0; num < 3; num++) {
+		//			System.out.println((Math.random()*3)+1);
+		//		}
+		//		for (int i = 1; i <= 5; i++) {
+		//			System.out.println(i);
+		//	}
+
+		//		for (int j = 1; j <= 5; j++) {
+		//			System.out.print(j);
+		//		}
+
+		//		for (int k = 0; k < 10; k++ ) {
+		//			System.out.println(k);
+		//		}
+
+		//		for (int h = 1; h < 11; h++) {
+		//			if(h%3 == 0) {
+		//				System.out.print(h);
+		//			}
+		//		}
+
+//		int result = 0;
+
+//		for (int z = 1; z <= 21; z++) {
+//			if(z%2 == 0) {
+//				result += z;
 //			}
 //		}
+//		System.out.print(result);
 		
-		int result = 0;
 		
-		for (int z = 0; z <= 20; z++) {
-			if(z%2 == 0) {
-				result += z;
-			}
+		for (int l = 10; l >= 1; l--) {
+			System.out.print(l);
 		}
-		System.out.print(result);
 	}
 
 }
--- exec/src/exec_01/switch_01.java
+++ exec/src/exec_01/switch_01.java
@@ -7,18 +7,18 @@ public class switch_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+
 		int score = 0;
 		String grade = null;
 		char plus = ' ';
-		
+
 		System.out.print("please, write your score : ");
 		score = sc.nextInt();
-		
+
 		switch(score / 10) {
 		case 10, 9 :
 			grade = "A";
-			break;
+		break;
 		case 8 :
 			grade = "B";
 			break;
@@ -28,11 +28,11 @@ public class switch_01 {
 		default :
 			grade = "F";
 		}
-		
+
 		switch(score % 10) {
 		case 9, 8, 7 :
 			plus = '+';
-			break;
+		break;
 		default : 
 			if(score == 100) {
 				grade = "A";
