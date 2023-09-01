582ff41c6455507f8c1b06d969049d812934d371
minkku <minkku@github.com>
2023-09-01 15:16:18 +0900

    practicing task

---
+++ exec/src/exec_01/tasktask.java
@@ -0,0 +1,32 @@
+package exec_01;
+
+public class tasktask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int numOfBucket = 0;
+//		
+//		numOfBucket = 10 % 123 + 3;
+//		System.out.printf("need bucket : %d", numOfBucket);
+//		
+//		char ch = 'A';
+//		System.out.print(ch);
+//		ch = (char)(ch + 2);
+//		System.out.print(ch);
+		
+		float f2 = 0.01f;
+		double d = 0.1;
+		
+		boolean result = (float)d == f2;
+		System.out.print(result);
+	}
+	
+
+}
+
+//num/100*100
+//num/10*10+1
+//10-num%10
+
+// (int)((5/9f -(fahrenheit-32))*100+0.5)/100f
+
