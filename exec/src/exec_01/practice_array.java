8a6308453acab2f3cea33fbb4ce0f543029f6d80
minkku <minkku@github.com>
2023-09-01 17:57:10 +0900

    adding array

--- exec/src/exec_01/practice_array.java
+++ exec/src/exec_01/practice_array.java
@@ -3,15 +3,40 @@ package exec_01;
 public class practice_array {
 
 	public static void main(String[] args) {
-		int[] a = {1,2,3,4,5};
-		int sum = 0;
-		
-		for(int i = 0; i < a.length; i++ ){
-			System.out.printf(">>>>> i 의 값 : %d\n", i);
-			System.out.printf(">>>>> a.length의 값 : %d\n", a.length);
-			sum += a[i];
-			System.out.println(sum);
+		//		int[] a = {1,2,3,4,5};
+		//		int sum = 0;
+		//		
+		//		for(int i = 0; i < a.length; i++ ){
+		//			System.out.printf(">>>>> i 의 값 : %d\n", i);
+		//			System.out.printf(">>>>> a.length의 값 : %d\n", a.length);
+		//			sum += a[i]; // sum = sum + a[i];
+		//			System.out.println(sum);
+		//		}
+
+		//		int[] score = {1,2,3,4};
+		//		
+		//		for(int i = 0; i < score.length; i++ ) {
+		//			System.out.println(score[i]);
+
+		//		int i ;
+		//		int[] score2 = new int[4];
+		//		for(i = 0; i < score2.length; i++) {
+		//			score2[i] = i + 1;
+		//			System.out.println(score2[i]);
+		//		}
+
+
+		int num[] = new int[7];
+
+		for(int i = 0; i < num.length; i++) {
+			num[i] = (int)(Math.random()*10) + 1;
+			if(i % 2 == 0 && i != 0) {
+				System.out.printf("%d ========\n", num[i]);
+			}
+			if(num[i] % 2 == 0 ) {
+				System.out.println(num[i]);
+			}
 		}
 	}
-
 }
+
