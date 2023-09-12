683733240e2289e1e88fbc051eae52416813c3c4
minkku <0106995@naver.com>
2023-09-12 12:11:20 +0900

    punch

--- exec/src/exec_01/class_practice.java
+++ exec/src/exec_01/class_practice.java
@@ -3,28 +3,40 @@ package exec_01;
 public class class_practice {
 
 	public static void main(String[] args) {
-		Hero h1 = new Hero();
-		
+		// default instance : 텅빈 생선자로 클래 내부에 아무것도 정의가 되지 않아있는경우 자동적으로 기입된다.
+		Hero h1 = new Hero();	// 객체를 생성 h1{name: null, hp: 0};
 		// 객체 값 초기화
-		h1.name = "이즈리얼";
-		h1.helth = 100;
-		h1.dmg = 350;
+		h1.name = "이즈리얼";		// 객체를 생성 h1{name: 이즈리얼, hp: 0};
+		h1.hp = 100;			// 객체를 생성 h1{name: 이즈리얼, hp: 100};
 		
 		// 객체에게 메소드 수행
-		h1.punch();
+		h1.punch();				// 객체를 생성 h1{name: 이즈리얼, hp: 100}.punch();
+								// 결과값 : [이즈리얼]의 펀치!
+		
+		Hero h2 = new Hero("간달프", 80); // 객체를 생성과 동시에 생성자를 통해서 초기화 시켜줌
+		h2.punch();
 	}
 }
 
 	class Hero {
 		// field
 		String name;
-		int helth;
-		int dmg;
+		int hp;
+
+		// 생성자
+		// 특징1 : 리턴 타입 명시가 없는 메소드
+		// 특징2 : 이름이 클래스명과 같다
+		Hero(){
+			
+		}
+		Hero(String _name, int _hp) {
+			name = _name;
+			hp = _hp;
+		}
 		
 		//method
 		void punch() {
-			 System.out.println(name + "의 펀치!");
-			 System.out.println(dmg + "dmg");
+			 System.out.printf("[%s]의 펀치!\n", name);
 		}
 	}
 
