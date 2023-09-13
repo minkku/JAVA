6c429957bdf4e952037d4521c4836a896adbeff5
minkku <minkku@github.com>
2023-09-13 17:34:18 +0900

    add class

--- exec/src/exec_01/ahhhhhhhh.java
+++ exec/src/exec_01/ahhhhhhhh.java
@@ -33,5 +33,36 @@ public class ahhhhhhhh {
 			
 			System.out.println("t1의 ch값은 " + t1.ch + " 입니다.");
 			System.out.println("t2의 ch값은 " + t2.ch + " 입니다.");
-	}
+			System.out.println();
+			
+			
+			
+			// 객체 배열
+			// Tv tv1, tv2, tv3;  == Tv[] tvArr = new Tv[3];
+			System.out.println("===== example 3 =====");
+			Tv tvArr[] = new Tv[3]; // 길이가 3인 Tv타입의 참조변수 배열
+			
+//			tvArr[0] = new Tv();
+//			tvArr[1] = new Tv();
+//			tvArr[2] = new Tv();
+			
+			// 배열의 초기화 블럭을 사용하면 한줄로 간단하게 표현 가능
+			// Tv tvArr[] = { new Tv(), new Tv(), new Tv() }
+			
+			// 다뤄야할 객체가 많을 경우 for문을 사용하면 된다.
+			// for(int i = 0; i < tvArr.length; i++) {
+			//			tvArr[i] = new Tv();
+			//}
+			
+			// Tv 객체를 생성해서 Tv 객체 배열의 각 요소에 저장
+			for(int i = 0; i < tvArr.length; i++) {
+				tvArr[i] = new Tv();
+				tvArr[i].ch = i + 10; // tvArr[i]의 ch에 i+10을 저장
+			}
+			
+			for (int i = 0; i < tvArr.length; i++) {
+				tvArr[i].channelUp();
+				System.out.printf("tvArr[%d].ch=%d%n",i,tvArr[i].ch);
+			}
+	}	// main의 끝
 }
