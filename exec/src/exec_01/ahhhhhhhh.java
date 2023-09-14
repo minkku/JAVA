b8f853701dd79cd7a9801224ece0ec7560860c87
minkku <0106995@naver.com>
2023-09-14 16:18:27 +0900

    recursive call

--- exec/src/exec_01/ahhhhhhhh.java
+++ exec/src/exec_01/ahhhhhhhh.java
@@ -52,6 +52,15 @@ class Card {
 	static int height = 250;
 }
 
+class FactorialTest {
+	static long factorial (int n) {
+		if(n <= 0 || n > 20) 
+			return -1;
+		if(n <= 1) return 1;
+		return n * factorial(n-1);
+	}
+}
+
 public class ahhhhhhhh {	
 	public static void main(String[] args) {
 			System.out.println("===== example 1 =====");
@@ -126,6 +135,17 @@ public class ahhhhhhhh {
 			System.out.println();
 			
 			System.out.println("===== example 6 =====");
+			int n = 21;
+			long result = 0;
 			
+			for(int i = 1; i <= n; i++) {
+				result = FactorialTest.factorial(i);
+				
+				if(result == -1) {
+					System.out.printf("유효하지 않은 값입니다.(0 < n <= 20:%d%n", n);
+					break;
+				}
+				System.out.printf("%2d!=%20d%n", i, result);
+			}
 	}	// main의 끝
 }
