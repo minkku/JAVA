c3c6c931bdb503e2c2164ba30b836b0079f5959c
minkku <0106995@naver.com>
2023-09-25 17:14:35 +0900

    getter n setter

--- exec/src/java01/Blackbox.java
+++ exec/src/java01/Blackbox.java
@@ -8,20 +8,27 @@ public class Blackbox {
 	int serialNum;
 	// static이 붙지않은것은 그냥 인스턴스 변수
 	
+	static int counter = 0; // 시리얼 번호를 생성해주는 역활 (처음에는 0 이였다가 ++ 연산자를 통해서 값 증가);
+	
 	static boolean canAutoReport = false; // 자동 신고 기능
 	// 여기에 static을 붙이게 되면 클래스 변수라고 함
 	// static 변수는 모두가 같은 기능을 공유해야할때 사용한다 그 외의 중복된 값이 들어가면 안되는 기능은
 	// 그냥 변수로 표기
 	
+	//can overloading
 	Blackbox() {
-		System.out.println("기본 생성자 호출");
+//		System.out.println("기본 생성자 호출");
+//		this.serialNum = ++counter;
+//		System.out.println("새로운 시리얼 넘버 : " + this.serialNum);
 	}
 	
 	Blackbox(String modelName, String resolution, int price, String color){
-		this.modelName = modelName;
-		this.resolution = resolution;
-		this.price = price;
-		this.color = color;
+//		this(); // this() 해주게 되면 자동으로 Blackbox(){} 을 사용할 수 있게된다.
+//		
+//		this.modelName = modelName;
+//		this.resolution = resolution;
+//		this.price = price;
+//		this.color = color;
 	}
 	
 	void autoReport() {
@@ -75,5 +82,37 @@ public class Blackbox {
 		this.modelName += modelName;
 		// 인스턴스 변수명과 매개변수 명이 똑같으면 this를 사용하면 된다.
 		// 만약 매개변수와 인스턴스 변수명이 다르면 굳이 this를 사용하지 않아도 된다.
+	}
+	
+	// getter & setter 
+	String getModelName() {
+		return modelName;
+	}
+	
+	void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+	
+	String getResolution() {
+		if(resolution == null || resolution.isEmpty()) {
+			return "판매자에게 문의하세요";
+		}
+		return resolution;
+	}
+	
+	void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+	
+	int getPrice() {
+		return price;
+	}
+	
+	void setPrice(int price) {
+		if(price < 100000) {
+			this.price = 100000;
+		} else {
+			this.price = price;
+		}
 	}
 }
